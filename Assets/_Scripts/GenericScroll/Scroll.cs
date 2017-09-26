2969e8035a2b16f302bdc738906d813e400b2dd4
Jacqui Bontigao <jbontiga@ucsd.edu>
2017-09-26 10:51:25 -0700

    fixed bug with rearrangement of objects on delete. scroll bar bug disappeared

--- Assets/_Scripts/GenericScroll/Scroll.cs
+++ Assets/_Scripts/GenericScroll/Scroll.cs
@@ -242,7 +242,6 @@ public class Scroll : MonoBehaviour
         }
     }
 
-    //BUG: need to handle rearrange when deleting from the middle (decide whether to adjust from top or bottom)
     public void deleteObjects(List<Transform> objs)
     {
         List<int> indeces = new List<int>();
@@ -278,7 +277,13 @@ public class Scroll : MonoBehaviour
         {
             highestVisIndex = objects.Count - 1;
 
-            if (numPages < prevNum) lowestVisIndex = lowestVisIndex - (fixedRowOrCol * (prevNum - numPages));
+            if (highestVisIndex - lowestVisIndex + 1 <= numberOfVisibleThings - fixedRowOrCol)
+            {
+                int offsetBy = (numberOfVisibleThings / fixedRowOrCol) - 
+                               (int)Mathf.Ceil(((float)(highestVisIndex - lowestVisIndex + 1) / (float)fixedRowOrCol));
+                lowestVisIndex = lowestVisIndex - (fixedRowOrCol * offsetBy);
+            } 
+
             if (numPages == 1) lowestVisIndex = 0;
         }
 
@@ -287,6 +292,8 @@ public class Scroll : MonoBehaviour
 
     private void moveObjects()
     {
+        if (objects == null || objects.Count == 0) return;
+
         if (((currDirection == direction.DOWN || currDirection == direction.RIGHT) && lowestVisIndex == 0) ||
             ((currDirection == direction.UP || currDirection == direction.LEFT) && highestVisIndex == objects.Count - 1))
             return;
