b44fe8cfd9dd32976c07d881efb3b347cd4f2c74
Ethan Vander Horn <edvander@ucsd.edu>
2017-10-05 12:07:35 -0700

    working on playback recording.

---
+++ Assets/_Scripts/PlayBack/ButtonLogger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLogger : Nanome.Core.Behaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/PlayBack/PlaybackLog.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class PlaybackLog : Nanome.Core.Behaviour
+{
+
+    static void SaveLog()
+    {
+        string savePath = Path.Combine(Path.Combine(
+            System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments),
+            "Calcflow"), "Recordings");
+
+        JsonUtility.ToJson(log);
+        //System.IO.File.WriteAllText(Path.Combine(savePath, fileName) + jsonExtension, json);
+
+    }
+
+    [SerializeField]
+    static Queue<PlayBackLogAction> log = new Queue<PlayBackLogAction>();
+
+    public static void LogMovement(float timestamp, Vector3 destination)
+    {
+        log.Enqueue(PlayBackLogAction.CreateMovement(timestamp, destination));
+    }
+
+    public static void LogButtonPress(float timestamp, GameObject presser)
+    {
+        log.Enqueue(PlayBackLogAction.CreateButtonPress(timestamp, presser));
+    }
+
+    [Serializable]
+    internal class PlayBackLogAction {
+
+        public enum ActionType
+        {
+            Movement,
+            ButtonPress
+        }
+        [SerializeField]
+        ActionType type;
+        [SerializeField]
+        float timeStamp;
+        [SerializeField]
+        Vector3 dest;
+        [SerializeField]
+        GameObject ButtonPresser;
+
+        internal void SetType(string type)
+        {
+            switch (type)
+            {
+                case "Movement":
+                    this.type = ActionType.Movement;
+                    break;
+                case "ButtonPress":
+                    this.type = ActionType.ButtonPress;
+                    break;
+                default:
+                    break;
+            }
+        }
+        internal void SetType(ActionType type)
+        {
+            this.type = type;
+        }
+
+        PlayBackLogAction()
+        {
+
+        }
+
+        internal static PlayBackLogAction CreateMovement(float timestamp, Vector3 destination)
+        {
+            PlayBackLogAction newAction = new PlayBackLogAction();
+            newAction.type = ActionType.Movement;
+            newAction.dest = destination;
+            return newAction;
+        }
+
+        internal static PlayBackLogAction CreateButtonPress(float timestamp, GameObject presser)
+        {
+            PlayBackLogAction newAction = new PlayBackLogAction();
+            newAction.type = ActionType.ButtonPress;
+            newAction.ButtonPresser = presser;
+            return newAction;
+        }
+
+    }
+}
