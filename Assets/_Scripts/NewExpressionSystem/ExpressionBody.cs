152864c023c528460853cd4a885d227370fd4bb6
Jacqui Bontigao <jbontiga@ucsd.edu>
2017-10-26 12:09:41 -0700

    started working on line feedback, need to test animation

--- Assets/_Scripts/NewExpressionSystem/ExpressionBody.cs
+++ Assets/_Scripts/NewExpressionSystem/ExpressionBody.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class ExpressionBody : QuickButton {
     Expressions expression;
+    Transform feedBack;
+
+    private bool menuActive = false;
+    private bool finishedScaling = false;
+    private bool retracting = false;
+
+    private Vector3 idleScale, selectedScale;
+
+    private IEnumerator scaleMenuUp, scaleMenuDown;
+    private IEnumerator backToSelected, backToIdle;
 
     protected override void Start()
     {
         base.Start();
         expression = GameObject.Find("Expressions").GetComponent<Expressions>();
+        feedBack = transform.parent.Find("Feedback");
+
+        selectedScale = new Vector3(4.56999f, 0.04f, 0.002f);
+        idleScale = new Vector3(0f, 0.04f, 0.002f);
     }
 
     protected override void ButtonEnterBehavior(GameObject other)
     {
+        if (menuActive)
+        {
+            if (retracting)
+            {
+                StopCoroutine(backToSelected);
+                retracting = false;
+            }
+
+            scaleMenuDown = ScaleTo(feedBack, feedBack.localScale, idleScale, 0.3f);
+            StartCoroutine(scaleMenuDown);
+        }
+        else
+        {
+            if (retracting)
+            {
+                StopCoroutine(backToIdle);
+                retracting = false;
+            }
+
+            scaleMenuUp = ScaleTo(feedBack, feedBack.localScale, selectedScale, 0.3f);
+            StartCoroutine(scaleMenuUp);
+        }
+
         ExpressionComponent expComp = transform.GetComponentInParent<ExpressionComponent>();
         if (expComp == null) {
             expComp = transform.parent.GetComponentInParent<ExpressionComponent>();
@@ -21,8 +58,41 @@ public class ExpressionBody : QuickButton {
         expression.setSelectedExpr(expComp.getExpressionParent());
     }
 
-    protected override void ButtonExitBehavior(GameObject other) { }
+    protected override void ButtonExitBehavior(GameObject other)
+    {
+        if (!finishedScaling)
+        {
+            if (menuActive)
+            {
+                StopCoroutine(scaleMenuDown);
+                backToSelected = ScaleTo(feedBack, feedBack.localScale, selectedScale, 0.5f);
+                StartCoroutine(backToSelected);
+            }
+            else
+            {
+                StopCoroutine(scaleMenuUp);
+                backToIdle = ScaleTo(feedBack, feedBack.localScale, idleScale, 0.5f);
+                StartCoroutine(backToIdle);
+            }
 
+            retracting = true;
+        }
+
+        finishedScaling = false;
+    }
+
+    IEnumerator ScaleTo(Transform obj, Vector3 start, Vector3 end, float overTime)
+    {
+        float startTime = Time.time;
+
+        while (Time.time < startTime + overTime)
+        {
+            obj.localScale = Vector3.Lerp(start, end, (Time.time - startTime) / overTime);
+            yield return null;
+        }
+
+        obj.localScale = end;
+    }
 
     void Update () { }
 }
