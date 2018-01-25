346373f7edf184973fc50afbffeec547056f7029
Jacqui Bontigao <jbontiga@ucsd.edu>
2018-01-25 14:58:35 -0800

    made UI for action toggle button for expressions

---
+++ Assets/_Scripts/NewExpressionSystem/ExpressionActions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpressionActions : QuickButton
+{
+    Transform delete;
+    Transform toggleHide;
+
+    private void Initialize()
+    {
+        delete = transform.parent.Find("Delete");
+        toggleHide = transform.parent.Find("ToggleHide");
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        Initialize();
+    }
+
+    protected override void ButtonEnterBehavior(GameObject other)
+    {
+
+    }
+
+    protected override void ButtonExitBehavior(GameObject other)
+    {
+
+    }
+
+    void Update() { }
+}
