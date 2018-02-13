fce423814d8d88556cb5f125c22c23f083ff9354
NanomeDev <dev@nanome.ai>
2018-02-13 15:17:29 -0800

    enters play mode from cmd line, sets hand positions but doesn't press buttons on its own

--- Assets/Editor/UnitTestBegin.cs
+++ Assets/Editor/UnitTestBegin.cs
@@ -3,12 +3,34 @@ using UnityEditor;
 using System;
 
 public  class UnitTestBegin : MonoBehaviour
-{ 
+{
     static void BeginTests()
     {
         //String[] arguments = Environment.GetCommandLineArgs();
         //UnityEngine.EditorSceneManagement.EditorSceneManager.OpenScene(3);
-        UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/_Scenes/3 - FreeParametrization.unity");
+
+        //opens a specific scene, could open scene 1 and try to click on a menu item
+        UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/_Scenes/1 - IntroScene.unity");
+        //locates a button on screen, so that we can find where we want to point to
+        GameObject curve = GameObject.Find("Parametrized Curve");
+
+        //Enters play mode
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+
+        Vector3 targetPos = curve.transform.position;
+        GameObject rHand = GameObject.Find("RightControllerPf");
+        RayCastSender sender = rHand.GetComponent<RayCastSender>();
+
+        //set the rotation to targetPos - current rotation, or some function to get the forward vector pointing to targetPos
+        rHand.transform.SetPositionAndRotation(new Vector3(10, 10, 10), Quaternion.Euler(new Vector3(0.35f, 0, 0)));
+        print(rHand.transform.position);
+        print(sender.TargetPoint);
+
+        // Spoof a trigger press ---- note this probably needs to happen after the next frame so that RayCastSender has time to update()
+        // OVRInput.Button.PrimaryIndexTrigger.
+
+       //could parse a JSON which declares different hand positions each update cycle, and tests interactions with gameobjects
+       //read the JSON filenames ( unit tests ) in as arguments using the getArg function
     }
 
     static string GetArg(string name)
@@ -22,5 +44,10 @@ public  class UnitTestBegin : MonoBehaviour
             }
         }
         return null;
+    }
+    
+    private void Update()
+    {
+        print("editor updates");
     }
 }   
