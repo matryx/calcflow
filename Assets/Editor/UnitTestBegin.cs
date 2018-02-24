37a3c1f19169898083dbde38a98c27c49bd2cee4
NanomeDev <dev@nanome.ai>
2018-02-23 16:11:20 -0800

    need to create json file to parse and unit test button presses

--- Assets/Editor/UnitTestBegin.cs
+++ Assets/Editor/UnitTestBegin.cs
@@ -11,13 +11,16 @@ public  class UnitTestBegin : MonoBehaviour
         //UnityEngine.EditorSceneManagement.EditorSceneManager.OpenScene(3);
 
         //opens a specific scene, could open scene 1 and try to click on a menu item
-
+        var sceneArray = new EditorBuildSettingsScene[2];
+        sceneArray[0] = new EditorBuildSettingsScene("Assets/_Scenes/1 - IntroScene.unity", true);
+        sceneArray[1] = new EditorBuildSettingsScene("Assets/_Scenes/8 - DoubleIntegral.unity", true);
+        EditorBuildSettings.scenes = sceneArray;
         UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/_Scenes/1 - IntroScene.unity");
         //locates a button on screen, so that we can find where we want to point to
 
         //Enters play mode
         EditorApplication.ExecuteMenuItem("Edit/Play");
-
+        print(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
 
         // Spoof a trigger press ---- note this probably needs to happen after the next frame so that RayCastSender has time to update()
         // OVRInput.Button.PrimaryIndexTrigger.
@@ -25,6 +28,12 @@ public  class UnitTestBegin : MonoBehaviour
         //could parse a JSON which declares different hand positions each update cycle, and tests interactions with gameobjects
         //read the JSON filenames ( unit tests ) in as arguments using the getArg function
         //test1();
+
+
+        //curve.GetComponent<TouchRayButton>().PressButton(curve);
+        print(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        
+
     }
 
     static string GetArg(string name)
@@ -41,7 +50,10 @@ public  class UnitTestBegin : MonoBehaviour
     }
     static private void Start()
     {
+        UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/_Scenes/11 - SphericalParam.unity");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Assets/_Scenes/11 - SphericalParam.unity");
 
+        /*
         GameObject curve = GameObject.Find("Parametrized Curve");
         Vector3 targetPos = curve.transform.position;
         GameObject rHand = GameObject.Find("RightControllerPf");
@@ -54,31 +66,15 @@ public  class UnitTestBegin : MonoBehaviour
         print(rHand.transform.position);
         print(sender.TargetPoint);
         print("editor updates");
+        */
     }
 
-    static void test1()
+    static void OnEnable()
     {
-        
-        if (Application.isPlaying && ind == 0)
-        {
-            GameObject curve = GameObject.Find("Parametrized Curve");
-            Vector3 targetPos = curve.transform.position;
-            GameObject rHand = GameObject.Find("RightControllerPf");
-            RayCastSender sender = rHand.GetComponent<RayCastSender>();
-
-            //set the rotation to targetPos - current rotation, or some function to get the forward vector pointing to targetPos
-            //rHand.transform.SetPositionAndRotation(new Vector3(0.1323853f, 1.618963f, 0.246521f), Quaternion.Euler(new Vector3(-10.206f, 23.222f,-12.707f)));
-            curve.GetComponent<TouchRayButton>().GetComponent<RayCastButton>().OnButtonEnter += curve.GetComponent<TouchRayButton>().GetComponent<RayCastButton>().PressButton;
-            curve.GetComponent<TouchRayButton>().GetComponent<RayCastButton>().PressButton(curve);
-            print(rHand.transform.position);
-            print(sender.TargetPoint);
-            print("editor updates");
-            ind++;
-        }
-        else
-        {
-            System.Threading.Thread.Sleep(500);
-            test1();
-        }
+        UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/_Scenes/11 - SphericalParam.unity");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Assets/_Scenes/11 - SphericalParam.unity");
+        GameObject curve = GameObject.Find("Parametrized Curve");
+        curve.GetComponent<TouchButton>().PressButton(curve);
+        print(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 }   
---
+++ Assets/loadonplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadonplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("Assets/_Scenes/8 - DoubleIntegral.unity");
+        GameObject dub = GameObject.Find("DoubleIntegralScene");
+        dub.GetComponent<TouchButton>().OnButtonEnter += dub.GetComponent<TouchButton>().PressButton;
+        dub.GetComponent<TouchButton>().PressButton(dub);
+        print(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
