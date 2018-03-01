f0f2e9ca1d038c5dd7f9c76c06988409ae1a7936
mukmai <deaconkum@ymail.com>
2018-03-01 11:02:50 -0800

    complete ExpressionSet.cs class for coords.

--- Assets/_Scripts/GraphScripts/ExpressionSet.cs
+++ Assets/_Scripts/GraphScripts/ExpressionSet.cs
@@ -15,8 +15,14 @@ public class ExpressionSet
         p, q, r, s, t, u, v, w
     }
 
+    public enum PtOptions
+    {
+        pt1, pt2, pt3
+    }
+
     public Dictionary<ExpOptions, Expression> expressions;
     public Dictionary<string, RangePair> ranges;
+    public Dictionary<string, PtCoord> ptCoords;
     public Dictionary<string, bool> expValidity = new Dictionary<string, bool>();
     public AK.ExpressionSolver solver = new AK.ExpressionSolver();
 
@@ -90,6 +96,45 @@ public class ExpressionSet
         }
     }
 
+    public void AddPtCoord(string variable, PtCoord ptCoord)
+    {
+        if (ptCoord != null)
+        {
+            if (ptCoords.ContainsKey(variable))
+            {
+                ptCoords[variable] = ptCoord;
+            }
+            else
+            {
+                ptCoords.Add(variable, ptCoord);
+            }
+        }
+    }
+    public void AddPtCoord(string variable, AxisCoord x, AxisCoord y, AxisCoord z)
+    {
+        PtCoord ptCoord = new PtCoord(x, y, z);
+        AddPtCoord(variable, ptCoord);
+    }
+    public void AddPtCoord(string variable, List<string> xTokens, List<string> yTokens, List<string> zTokens)
+    {
+        AxisCoord x = new AxisCoord(xTokens);
+        AxisCoord y = new AxisCoord(yTokens);
+        AxisCoord z = new AxisCoord(zTokens);
+        AddPtCoord(variable, x, y, z);
+    }
+    public void AddPtCoord(string variable)
+    {
+        AddPtCoord(variable, new List<string>(), new List<string>(), new List<string>());
+    }
+
+    public void RemovePtCoord(string variable)
+    {
+        if (ptCoords.ContainsKey(variable))
+        {
+            ptCoords.Remove(variable);
+        }
+    }
+
     public ExpressionSet()
     {
         expressions = new Dictionary<ExpOptions, Expression>();
@@ -102,6 +147,11 @@ public class ExpressionSet
         AddRange("u");
         AddRange("v");
         AddRange("w");
+
+        ptCoords = new Dictionary<string, PtCoord>();
+        AddPtCoord("pt1");
+        AddPtCoord("pt2");
+        AddPtCoord("pt3");
     }
 
     public ExpressionSet DeepCopy()
@@ -119,6 +169,12 @@ public class ExpressionSet
             newEs.ranges.Add(key, new RangePair(ranges[key]));
         }
 
+        newEs.ptCoords = new Dictionary<string, PtCoord>();
+        foreach (string key in ptCoords.Keys)
+        {
+            newEs.ptCoords.Add(key, new PtCoord(ptCoords[key]));
+        }
+
         newEs.expValidity = new Dictionary<string, bool>(expValidity);
 
         return newEs;
@@ -130,23 +186,30 @@ public class ExpressionSet
 
         newEs.expressions = new Dictionary<ExpOptions, Expression>(expressions);
         newEs.ranges = new Dictionary<string, RangePair>((Dictionary<string, RangePair>)ranges);
+        newEs.ptCoords = new Dictionary<string, PtCoord>((Dictionary<string, PtCoord>)ptCoords);
         newEs.expValidity = new Dictionary<string, bool>(expValidity);
 
         return newEs;
     }
 
-    internal ExpressionSet (string[] rangeKeys, List<RangePair> rangePairs, ExpOptions[] ExpressionKeys, List<Expression> ExpressionValues)
+    internal ExpressionSet (string[] rangeKeys, List<RangePair> rangePairs, string[] ptKeys, List<PtCoord> pts, ExpOptions[] expressionKeys, List<Expression> expressionValues)
     {
         ranges = new Dictionary<string, RangePair>();
-        for (int i =0; i < rangePairs.Count; i++)
+        for (int i = 0; i < rangePairs.Count; i++)
         {
             ranges.Add(rangeKeys[i], rangePairs[i]);
         }
 
         expressions = new Dictionary<ExpOptions, Expression>();
-        for (int i = 0; i < ExpressionValues.Count; i++)
+        for (int i = 0; i < expressionValues.Count; i++)
         {
-            expressions.Add(ExpressionKeys[i], ExpressionValues[i]);
+            expressions.Add(expressionKeys[i], expressionValues[i]);
+        }
+
+        ptCoords = new Dictionary<string, PtCoord>();
+        for (int i = 0; i < pts.Count; i++)
+        {
+            ptCoords.Add(ptKeys[i], pts[i]);
         }
     }
 
@@ -161,6 +224,17 @@ public class ExpressionSet
             ranges[RO].Max.compileTokens();
             expValidity[RO] &= ranges[RO].Max.GenerateAKSolver(solver);
             isValid &= expValidity[RO];
+        }
+        foreach (string PO in ptCoords.Keys)
+        {
+            // solver.SetGlobalVariable(RO, -666);
+            ptCoords[PO].X.compileTokens();
+            expValidity[PO] = ptCoords[PO].X.GenerateAKSolver(solver);
+            ptCoords[PO].Y.compileTokens();
+            expValidity[PO] &= ptCoords[PO].Y.GenerateAKSolver(solver);
+            ptCoords[PO].Z.compileTokens();
+            expValidity[PO] &= ptCoords[PO].Z.GenerateAKSolver(solver);
+            isValid &= expValidity[PO];
         }
         foreach (ExpOptions EX in expressions.Keys)
         {
@@ -184,6 +258,13 @@ public class ExpressionSet
             Debug.Log(ro);
             ranges[ro].Min.PrintOut();
             ranges[ro].Max.PrintOut();
+        }
+        foreach (string po in ptCoords.Keys)
+        {
+            Debug.Log(po);
+            ptCoords[po].X.PrintOut();
+            ptCoords[po].Y.PrintOut();
+            ptCoords[po].Z.PrintOut();
         }
     }
 
@@ -503,7 +584,6 @@ public class AxisCoord : CalcOutput
         }
         return success;
     }
-
 }
 
 [System.Serializable]
@@ -511,6 +591,9 @@ public class SerializableExpressionSet
 {
     public string[] rangeKeys;
     public List<SerializableRangePair> rangePairs = new List<SerializableRangePair>();
+
+    public string[] ptKeys;
+    public  List<SerializablePtCoord> pts = new List<SerializablePtCoord>();
 
     public ExpressionSet.ExpOptions[] ExpressionKeys;
     public List<string> ExpressionValues = new List<string>();
@@ -523,6 +606,12 @@ public class SerializableExpressionSet
         {
             rangePairs.Add(new SerializableRangePair(es.ranges[key]));
         }
+        ptKeys = new string[es.ptCoords.Count];
+        es.ptCoords.Keys.CopyTo(ptKeys, 0);
+        foreach (string key in ptKeys)
+        {
+            pts.Add(new SerializablePtCoord(es.ptCoords[key]));
+        }
         ExpressionKeys = new ExpressionSet.ExpOptions[es.expressions.Count];
         es.expressions.Keys.CopyTo(ExpressionKeys, 0);
         foreach (ExpressionSet.ExpOptions key in ExpressionKeys)
@@ -533,12 +622,16 @@ public class SerializableExpressionSet
 
     public ExpressionSet ConvertToExpressionSet()
     {
-        return new ExpressionSet(rangeKeys, DeserializeRangePairs(), ExpressionKeys, DeserializeExpression());
+        return new ExpressionSet(rangeKeys, DeserializeRangePairs(), ptKeys, DeserializePtCoords(), ExpressionKeys, DeserializeExpression());
     }
 
     private List<RangePair> DeserializeRangePairs()
     {
         return rangePairs.Select(x => x.Deserialize()).ToList();
+    }
+    private List<PtCoord> DeserializePtCoords()
+    {
+        return pts.Select(x => x.Deserialize()).ToList();
     }
     private List<Expression> DeserializeExpression()
     {
@@ -581,5 +674,42 @@ public class SerializableRange
         Range rn = new Range(ExpressionParser.Parse(rawText));
         rn.Exclusive = exclusive;
         return rn;
+    }
+}
+
+[System.Serializable]
+public class SerializablePtCoord
+{
+    public SerializableAxisCoord X;
+    public SerializableAxisCoord Y;
+    public SerializableAxisCoord Z;
+
+    public SerializablePtCoord(PtCoord ptCoord)
+    {
+        X = new SerializableAxisCoord(ptCoord.X);
+        Y = new SerializableAxisCoord(ptCoord.Y);
+        Z = new SerializableAxisCoord(ptCoord.Z);
+    }
+
+    public PtCoord Deserialize()
+    {
+        return new PtCoord(X.Deserialize(), Y.Deserialize(), Z.Deserialize());
+    }
+}
+
+[System.Serializable]
+public class SerializableAxisCoord
+{
+    public string rawText;
+
+    public SerializableAxisCoord(AxisCoord axisCoord)
+    {
+        this.rawText = axisCoord.rawText;
+    }
+
+    public AxisCoord Deserialize()
+    {
+        AxisCoord ac = new AxisCoord(ExpressionParser.Parse(rawText));
+        return ac;
     }
 }
