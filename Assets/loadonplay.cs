6aefb7a0d20630486c8fe2808365dfb47bf47de3
NanomeDev <dev@nanome.ai>
2018-03-01 19:10:21 -0800

    Almost fakes the button press

--- Assets/loadonplay.cs
+++ Assets/loadonplay.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class loadonplay : MonoBehaviour {
@@ -8,9 +9,19 @@ public class loadonplay : MonoBehaviour {
 	void Start () {
         //UnityEngine.SceneManagement.SceneManager.LoadScene("Assets/_Scenes/8 - DoubleIntegral.unity");
         GameObject dub = GameObject.Find("DoubleIntegralScene");
-        dub.GetComponent<TouchButton>().OnButtonEnter += dub.GetComponent<TouchButton>().PressButton;
-        dub.GetComponent<TouchButton>().PressButton(dub);
+        // dub.GetComponent<TouchButton>().OnButtonEnter += dub.GetComponent<TouchButton>().PressButton;
+        RayCastButton rcButton = dub.GetComponent<RayCastButton>();
+       // dub.GetComponent<TouchRayButton>().onClick.invoke();
+       RayCastButton button = rcButton;
+        if (button != null)
+        {
+            button.PressButton(dub);
+        }
+        //ExecuteEvents.Execute(dub.GetComponent<LoadSceneOnTouch>());
+        // dub.GetComponent<TouchButton>().PressButton(dub);
+        //dub.GetComponent<TouchButton>().
         print(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        print("inside tester");
     }
 	
 	// Update is called once per frame
