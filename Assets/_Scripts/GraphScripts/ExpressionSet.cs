cd0f2ac1d5218b97872563bf1a4cde574c44800d
mukmai <deaconkum@ymail.com>
2018-03-07 14:50:21 -0800

    Create new scripts for calculator (not tested)

--- Assets/_Scripts/GraphScripts/ExpressionSet.cs
+++ Assets/_Scripts/GraphScripts/ExpressionSet.cs
@@ -15,14 +15,8 @@ public class ExpressionSet
         p, q, r, s, t, u, v, w
     }
 
-    public enum PtOptions
-    {
-        pt1, pt2, pt3
-    }
-
     public Dictionary<ExpOptions, Expression> expressions;
     public Dictionary<string, RangePair> ranges;
-    public Dictionary<string, PtCoord> ptCoords;
     public Dictionary<string, bool> expValidity = new Dictionary<string, bool>();
     public AK.ExpressionSolver solver = new AK.ExpressionSolver();
 
@@ -96,45 +90,6 @@ public class ExpressionSet
         }
     }
 
-    public void AddPtCoord(string variable, PtCoord ptCoord)
-    {
-        if (ptCoord != null)
-        {
-            if (ptCoords.ContainsKey(variable))
-            {
-                ptCoords[variable] = ptCoord;
-            }
-            else
-            {
-                ptCoords.Add(variable, ptCoord);
-            }
-        }
-    }
-    public void AddPtCoord(string variable, AxisCoord x, AxisCoord y, AxisCoord z)
-    {
-        PtCoord ptCoord = new PtCoord(x, y, z);
-        AddPtCoord(variable, ptCoord);
-    }
-    public void AddPtCoord(string variable, List<string> xTokens, List<string> yTokens, List<string> zTokens)
-    {
-        AxisCoord x = new AxisCoord(xTokens);
-        AxisCoord y = new AxisCoord(yTokens);
-        AxisCoord z = new AxisCoord(zTokens);
-        AddPtCoord(variable, x, y, z);
-    }
-    public void AddPtCoord(string variable)
-    {
-        AddPtCoord(variable, new List<string>(), new List<string>(), new List<string>());
-    }
-
-    public void RemovePtCoord(string variable)
-    {
-        if (ptCoords.ContainsKey(variable))
-        {
-            ptCoords.Remove(variable);
-        }
-    }
-
     public ExpressionSet()
     {
         expressions = new Dictionary<ExpOptions, Expression>();
@@ -147,11 +102,6 @@ public class ExpressionSet
         AddRange("u");
         AddRange("v");
         AddRange("w");
-
-        ptCoords = new Dictionary<string, PtCoord>();
-        AddPtCoord("pt1");
-        AddPtCoord("pt2");
-        AddPtCoord("pt3");
     }
 
     public ExpressionSet DeepCopy()
@@ -169,12 +119,6 @@ public class ExpressionSet
             newEs.ranges.Add(key, new RangePair(ranges[key]));
         }
 
-        newEs.ptCoords = new Dictionary<string, PtCoord>();
-        foreach (string key in ptCoords.Keys)
-        {
-            newEs.ptCoords.Add(key, new PtCoord(ptCoords[key]));
-        }
-
         newEs.expValidity = new Dictionary<string, bool>(expValidity);
 
         return newEs;
@@ -186,30 +130,23 @@ public class ExpressionSet
 
         newEs.expressions = new Dictionary<ExpOptions, Expression>(expressions);
         newEs.ranges = new Dictionary<string, RangePair>((Dictionary<string, RangePair>)ranges);
-        newEs.ptCoords = new Dictionary<string, PtCoord>((Dictionary<string, PtCoord>)ptCoords);
         newEs.expValidity = new Dictionary<string, bool>(expValidity);
 
         return newEs;
     }
 
-    internal ExpressionSet (string[] rangeKeys, List<RangePair> rangePairs, string[] ptKeys, List<PtCoord> pts, ExpOptions[] expressionKeys, List<Expression> expressionValues)
+    internal ExpressionSet (string[] rangeKeys, List<RangePair> rangePairs, ExpOptions[] ExpressionKeys, List<Expression> ExpressionValues)
     {
         ranges = new Dictionary<string, RangePair>();
-        for (int i = 0; i < rangePairs.Count; i++)
+        for (int i =0; i < rangePairs.Count; i++)
         {
             ranges.Add(rangeKeys[i], rangePairs[i]);
         }
 
         expressions = new Dictionary<ExpOptions, Expression>();
-        for (int i = 0; i < expressionValues.Count; i++)
+        for (int i = 0; i < ExpressionValues.Count; i++)
         {
-            expressions.Add(expressionKeys[i], expressionValues[i]);
-        }
-
-        ptCoords = new Dictionary<string, PtCoord>();
-        for (int i = 0; i < pts.Count; i++)
-        {
-            ptCoords.Add(ptKeys[i], pts[i]);
+            expressions.Add(ExpressionKeys[i], ExpressionValues[i]);
         }
     }
 
@@ -224,17 +161,6 @@ public class ExpressionSet
             ranges[RO].Max.compileTokens();
             expValidity[RO] &= ranges[RO].Max.GenerateAKSolver(solver);
             isValid &= expValidity[RO];
-        }
-        foreach (string PO in ptCoords.Keys)
-        {
-            // solver.SetGlobalVariable(RO, -666);
-            ptCoords[PO].X.compileTokens();
-            expValidity[PO] = ptCoords[PO].X.GenerateAKSolver(solver);
-            ptCoords[PO].Y.compileTokens();
-            expValidity[PO] &= ptCoords[PO].Y.GenerateAKSolver(solver);
-            ptCoords[PO].Z.compileTokens();
-            expValidity[PO] &= ptCoords[PO].Z.GenerateAKSolver(solver);
-            isValid &= expValidity[PO];
         }
         foreach (ExpOptions EX in expressions.Keys)
         {
@@ -258,13 +184,6 @@ public class ExpressionSet
             Debug.Log(ro);
             ranges[ro].Min.PrintOut();
             ranges[ro].Max.PrintOut();
-        }
-        foreach (string po in ptCoords.Keys)
-        {
-            Debug.Log(po);
-            ptCoords[po].X.PrintOut();
-            ptCoords[po].Y.PrintOut();
-            ptCoords[po].Z.PrintOut();
         }
     }
 
@@ -511,89 +430,10 @@ public class Range : CalcOutput
 }
 
 [System.Serializable]
-public class PtCoord
-{
-    public AxisCoord X;
-    public AxisCoord Y;
-    public AxisCoord Z;
-
-    public PtCoord(AxisCoord x, AxisCoord y, AxisCoord z)
-    {
-        X = x;
-        Y = y;
-        Z = z;
-    }
-
-    public PtCoord(PtCoord toCopy)
-    {
-        X = new AxisCoord(toCopy.X);
-        Y = new AxisCoord(toCopy.Y);
-        Z = new AxisCoord(toCopy.Z);
-    }
-}
-
-[System.Serializable]
-public class AxisCoord : CalcOutput
-{
-    float val;
-
-    public float Value
-    {
-        get
-        {
-            return val;
-        }
-    }
-
-    public string expression
-    {
-        get
-        {
-            return rawText;
-        }
-        set
-        {
-            rawText = value;
-        }
-    }
-
-    public AxisCoord(AxisCoord toCopy)
-    {
-        this.rawText = toCopy.rawText;
-        this.tokens = new List<string>(toCopy.tokens);
-    }
-
-    public AxisCoord(List<string> tokens)
-    {
-        rawText = "";
-        this.tokens = tokens;
-    }
-
-    public AxisCoord()
-    {
-        rawText = "";
-        tokens = new List<string>();
-    }
-
-    public override bool GenerateAKSolver(AK.ExpressionSolver solver)
-    {
-        bool success = base.GenerateAKSolver(solver);
-        if (success)
-        {
-            val = (float)AKExpression.Evaluate();
-        }
-        return success;
-    }
-}
-
-[System.Serializable]
 public class SerializableExpressionSet
 {
     public string[] rangeKeys;
     public List<SerializableRangePair> rangePairs = new List<SerializableRangePair>();
-
-    public string[] ptKeys;
-    public  List<SerializablePtCoord> pts = new List<SerializablePtCoord>();
 
     public ExpressionSet.ExpOptions[] ExpressionKeys;
     public List<string> ExpressionValues = new List<string>();
@@ -606,12 +446,6 @@ public class SerializableExpressionSet
         {
             rangePairs.Add(new SerializableRangePair(es.ranges[key]));
         }
-        ptKeys = new string[es.ptCoords.Count];
-        es.ptCoords.Keys.CopyTo(ptKeys, 0);
-        foreach (string key in ptKeys)
-        {
-            pts.Add(new SerializablePtCoord(es.ptCoords[key]));
-        }
         ExpressionKeys = new ExpressionSet.ExpOptions[es.expressions.Count];
         es.expressions.Keys.CopyTo(ExpressionKeys, 0);
         foreach (ExpressionSet.ExpOptions key in ExpressionKeys)
@@ -622,16 +456,12 @@ public class SerializableExpressionSet
 
     public ExpressionSet ConvertToExpressionSet()
     {
-        return new ExpressionSet(rangeKeys, DeserializeRangePairs(), ptKeys, DeserializePtCoords(), ExpressionKeys, DeserializeExpression());
+        return new ExpressionSet(rangeKeys, DeserializeRangePairs(), ExpressionKeys, DeserializeExpression());
     }
 
     private List<RangePair> DeserializeRangePairs()
     {
         return rangePairs.Select(x => x.Deserialize()).ToList();
-    }
-    private List<PtCoord> DeserializePtCoords()
-    {
-        return pts.Select(x => x.Deserialize()).ToList();
     }
     private List<Expression> DeserializeExpression()
     {
@@ -674,42 +504,5 @@ public class SerializableRange
         Range rn = new Range(ExpressionParser.Parse(rawText));
         rn.Exclusive = exclusive;
         return rn;
-    }
-}
-
-[System.Serializable]
-public class SerializablePtCoord
-{
-    public SerializableAxisCoord X;
-    public SerializableAxisCoord Y;
-    public SerializableAxisCoord Z;
-
-    public SerializablePtCoord(PtCoord ptCoord)
-    {
-        X = new SerializableAxisCoord(ptCoord.X);
-        Y = new SerializableAxisCoord(ptCoord.Y);
-        Z = new SerializableAxisCoord(ptCoord.Z);
-    }
-
-    public PtCoord Deserialize()
-    {
-        return new PtCoord(X.Deserialize(), Y.Deserialize(), Z.Deserialize());
-    }
-}
-
-[System.Serializable]
-public class SerializableAxisCoord
-{
-    public string rawText;
-
-    public SerializableAxisCoord(AxisCoord axisCoord)
-    {
-        this.rawText = axisCoord.rawText;
-    }
-
-    public AxisCoord Deserialize()
-    {
-        AxisCoord ac = new AxisCoord(ExpressionParser.Parse(rawText));
-        return ac;
     }
 }
---
+++ Assets/_Scripts/GraphScripts/PtManager.cs
@@ -0,0 +1,301 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PtManager : MonoBehaviour
+{
+    PtSet currPtSet;
+    CalcOutput currOutput;
+
+    [HideInInspector]
+    public bool inputReceived;
+
+    [HideInInspector]
+    public PtSet ptSet;
+
+    [HideInInspector]
+    public SaveLoadMenu saveLoadMenu;
+
+    private PtInput ptInput;
+    private PieceWiseControl pieceWiseControl;
+    private BoundsManager boundsManager;
+
+    private Color positiveFeedback = new Color(0, 204, 54);
+    private Color negativeFeedback = Color.red;
+
+    int maxDisplayLength = 7;
+
+    internal bool toExport = false;
+
+    [SerializeField]
+    public FlexActionableComponent defaultSpeed;
+    public FlexActionableComponent defaultEffect;
+
+    [SerializeField]
+    ConnectedMenus connectedMenus;
+
+    [SerializeField]
+    FeedBacks feedbacks;
+
+    [SerializeField]
+    Inputs inputs;
+
+    [System.Serializable]
+    internal class ConnectedMenus
+    {
+        [SerializeField]
+        internal PtInput ptInput;
+
+        //[SerializeField]
+        // this is for tabs
+        //internal PieceWiseControl pieceWiseControl;
+
+        //[SerializeField]
+        // this is for handling preset (e.g. circle, cone)
+        //internal PresetMenu presetMenu;
+        [SerializeField]
+        internal PtOutputMenu ptOutputMenu;
+
+        //[SerializeField]
+        // this is for boundary variables
+        //internal BoundsManager boundsManager;
+
+        //[SerializeField]
+        // this is for save and load
+        //internal SaveLoadMenu saveLoadMenu;
+
+        //[SerializeField]
+        // this is for controlling effect of particle animation
+        //internal ParticleAnimationSettings particleAnimationSettings;
+    }
+
+    [System.Serializable]
+    internal class FeedBacks
+    {
+        [SerializeField]
+        internal Renderer pt1Feedback;
+        [SerializeField]
+        internal Renderer pt2Feedback;
+        [SerializeField]
+        internal Renderer pt3Feedback;
+    }
+
+    [System.Serializable]
+    internal class Inputs
+    {
+        [SerializeField]
+        internal TextMesh pt1XInput, pt1YInput, pt1ZInput,
+                pt2XInput, pt2YInput, pt2ZInput,
+                pt3XInput, pt3YInput, pt3ZInput;
+    }
+
+    public void ChangePtSet(PtSet PS)
+    {
+        ptSet = PS;
+        ptInput.ChangeOutput(ptSet.ptCoords["pt1"].X);
+        manageText();
+        if (boundsManager != null) boundsManager.UpdateButtonText();
+    }
+
+    //TODO: how to save and load
+    /*
+    public void LoadSavedExpressionSets(List<ExpressionSet> expressionSets)
+    {
+        List<ExpressionSet> ess = new List<ExpressionSet>();
+        for (int i = 0; i < expressionSets.Count; i++)
+        {
+            ess.Add(expressionSets[i].DeepCopy());
+            ess[ess.Count - 1].CompileAll();
+        }
+        paramSurface.expressionSets = ess;
+        pieceWiseControl.ForceNumberOfTabs(ess.Count);
+        expressionSet = paramSurface.expressionSets[0];
+        calcInput.ChangeOutput(expressionSet.expressions[X]);
+        if(boundsManager != null) boundsManager.UpdateButtonText();
+        inputReceived = true;
+    }
+    */
+
+    public void SetOutput(CalcOutput output)
+    {
+        ptInput.ChangeOutput(output);
+    }
+
+    private void Initialize()
+    {
+        ptInput = connectedMenus.ptInput;
+        //boundsManager = connectedMenus.boundsManager;
+        //pieceWiseControl = connectedMenus.pieceWiseControl;
+        //saveLoadMenu = connectedMenus.saveLoadMenu;
+
+        //if (connectedMenus.boundsManager != null) connectedMenus.boundsManager.Initialize(this);
+        connectedMenus.ptInput.Initialize(this);
+        //tier 3
+        connectedMenus.ptOutputMenu.Initialize(this);
+
+        //Req: calcInput
+        //connectedMenus.pieceWiseControl.Initialize(this);
+        //Req: calcInput
+        // Initialize ptSet (not necessary if implemented tabs)
+        ptSet = new PtSet();
+        ptInput.ChangeOutput(ptSet.ptCoords["pt1"].X);
+        //Req: calcInput
+        //connectedMenus.presetMenu.Initialize(this);
+
+        //connectedMenus.saveLoadMenu.Initialize(this);
+
+        //if (connectedMenus.particleAnimationSettings != null)
+        //    connectedMenus.particleAnimationSettings.Initialize(this);
+    }
+
+    public void PresetPressed()
+    {
+        ptInput.ChangeOutput(ptSet.ptCoords["pt1"].X);
+        if (boundsManager != null) boundsManager.UpdateButtonText();
+        inputReceived = true;
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        Initialize();
+    }
+
+    public bool updateOverlay = false;
+    public bool updateText = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (updateText || inputReceived)
+        {
+            manageText();
+            updateText = false;
+        }
+
+        if (inputReceived)
+        {
+            inputReceived = false;
+            updateOverlay = true;
+            bool isValid = ptSet.CompileAll();
+            ManageFeedback();
+            //if (isValid)
+                //paramSurface.GenerateParticles();
+        }
+        if (toExport)
+        {
+            toExport = false;
+            //paramSurface.GenerateMesh();
+        }
+    }
+
+    public void ManageFeedback()
+    {
+        if (feedbacks.pt1Feedback != null) feedbacks.pt1Feedback.material.color = ptSet.expValidity["pt1"] ? positiveFeedback : negativeFeedback;
+        if (feedbacks.pt2Feedback != null) feedbacks.pt2Feedback.material.color = ptSet.expValidity["pt2"] ? positiveFeedback : negativeFeedback;
+        if (feedbacks.pt3Feedback != null) feedbacks.pt3Feedback.material.color = ptSet.expValidity["pt3"] ? positiveFeedback : negativeFeedback;
+    }
+
+    public void manageText()
+    {
+        #region coords
+        if (ptSet.ptCoords.ContainsKey("pt1") && inputs.pt1XInput != null)
+        {
+            inputs.pt1XInput.text = displayText(ptSet.ptCoords["pt1"].X.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt1"].X, maxDisplayLength);
+            inputs.pt1YInput.text = displayText(ptSet.ptCoords["pt1"].Y.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt1"].Y, maxDisplayLength);
+            inputs.pt1ZInput.text = displayText(ptSet.ptCoords["pt1"].Z.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt1"].Z, maxDisplayLength);
+        }
+        if (ptSet.ptCoords.ContainsKey("pt2") && inputs.pt2XInput != null)
+        {
+            inputs.pt2XInput.text = displayText(ptSet.ptCoords["pt2"].X.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt2"].X, maxDisplayLength);
+            inputs.pt2YInput.text = displayText(ptSet.ptCoords["pt2"].Y.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt2"].Y, maxDisplayLength);
+            inputs.pt2ZInput.text = displayText(ptSet.ptCoords["pt2"].Z.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt2"].Z, maxDisplayLength);
+        }
+        if (ptSet.ptCoords.ContainsKey("pt3") && inputs.pt3XInput != null)
+        {
+            inputs.pt3XInput.text = displayText(ptSet.ptCoords["pt3"].X.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt3"].X, maxDisplayLength);
+            inputs.pt3YInput.text = displayText(ptSet.ptCoords["pt3"].Y.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt3"].Y, maxDisplayLength);
+            inputs.pt3ZInput.text = displayText(ptSet.ptCoords["pt3"].Z.tokens, ptInput.index, ptInput.currExpression == ptSet.ptCoords["pt3"].Z, maxDisplayLength);
+        }
+        #endregion
+    }
+
+    public string displayText(List<string> exp, int index0, bool mark, int displayLength)
+    {
+        bool end = false;
+        bool start = false;
+        int forward = 1;
+        int back = 0;
+        int space = displayLength;
+        if (!mark)
+        {
+            index0 = exp.Count;
+        }
+        string displayList = (mark) ? "_" : "";
+        while (!start || !end)
+        {
+            if (!end && index0 + forward - 1 < exp.Count)
+            {
+                string next = exp[index0 + forward - 1];
+                next = CleanRepresentation(next);
+                if (space - next.Length > 0)
+                {
+                    displayList += (next);
+                    space -= next.Length;
+                    forward++;
+                }
+                else
+                {
+                    displayList += "...";
+                    end = true;
+                }
+            }
+            else end = true;
+            if (!start && index0 - back > 0)
+            {
+                string prev = exp[index0 - back - 1];
+                if (prev == "pi")
+                {
+                    prev = "π";
+                }
+                if (space - prev.Length > 0)
+                {
+                    displayList = prev + displayList;
+                    space -= prev.Length;
+                    back++;
+                }
+                else
+                {
+                    displayList = "..." + displayList;
+                    start = true;
+                }
+            }
+            else start = true;
+        }
+        //currText.text = displayList;
+        return displayList;
+    }
+
+    string CleanRepresentation(string input)
+    {
+
+        switch (input)
+        {
+            case "pi":
+                return "π";
+            case "arccos":
+                return "acos";
+            case "arcsin":
+                return "asin";
+            case "arctan":
+                return "atan";
+            case "arccosh":
+                return "acosh";
+            case "arcsinh":
+                return "asinh";
+            case "arctanh":
+                return "atanh";
+            default:
+                return input;
+        }
+    }
+}
