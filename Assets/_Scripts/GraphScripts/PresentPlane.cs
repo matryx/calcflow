145422e152d47e18f0f7fa28d085bcc14f62e3f2
mukmai <deaconkum@ymail.com>
2018-03-09 12:49:05 -0800

    starting midpoint of the triangle

--- Assets/_Scripts/GraphScripts/PresentPlane.cs
+++ Assets/_Scripts/GraphScripts/PresentPlane.cs
@@ -15,7 +15,10 @@ public class PresentPlane : MonoBehaviour {
 
 	public Vector3 vector12;
 	public Vector3 vector13;
+	public Vector3 vector23;
 	public Vector3 normalVector;
+
+	public string rawEquation;
 
 	AK.ExpressionSolver solver;
 
@@ -44,9 +47,14 @@ public class PresentPlane : MonoBehaviour {
 
 	public void ApplyGraphAdjustment()
 	{
+		vector23 = generateVector(rawPt2, rawPt3);
 		//TODO: find midpoint of triangle
 	}
 
+	public float generateModule (Vector3 myVector) 
+	{
+		return Mathf.Sqrt(myVector.x*myVector.x + myVector.y*myVector.y + myVector.z*myVector.z);
+	}
 	// return vector from pt1 to pt2
 	public Vector3 generateVector(PtCoord pt1, PtCoord pt2)
 	{
@@ -57,11 +65,19 @@ public class PresentPlane : MonoBehaviour {
 		return result;
 	}
 
-	public void calculatePlane() {
+	// Return the raw string of the equation
+	public string  calculatePlane() {
 		vector12 = generateVector(rawPt1, rawPt2);
 		vector13 = generateVector(rawPt1, rawPt3);
+		// Calculate the normal vector
 		normalVector.x = vector12.y * vector13.z - vector12.z * vector13.y;
 		normalVector.y = -(vector12.x * vector13.z - vector12.z * vector13.x);
 		normalVector.z = vector12.x * vector13.y - vector12.y * vector13.x;
+		
+		// Basic formula of the equation
+		rawEquation = rawPt1.X.Value + "* ( x -" + normalVector.x + ") + " +  
+			rawPt1.Y.Value + "* ( y -" + normalVector.y + ") + " +  rawPt1.Z.Value + "* ( x -" + normalVector.z + ") = 0";
+
+		return rawEquation;
 	}
 }
