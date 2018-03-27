f859ed778c3c16b92953cc8092a807459a0de546
mukmai <deaconkum@ymail.com>
2018-03-26 17:07:03 -0700

    Fixed bug for graph showing NaN and started on displaying points on graph.

--- Assets/_Scripts/GraphScripts/PresentPlane.cs
+++ Assets/_Scripts/GraphScripts/PresentPlane.cs
@@ -19,7 +19,7 @@ public class PresentPlane : MonoBehaviour {
 	private PtCoord rawPt2;
 	private PtCoord rawPt3;
 
-	Vector3 center;
+	public Vector3 center;
 	
 
 	public PtSet ptSet;
@@ -85,26 +85,28 @@ public class PresentPlane : MonoBehaviour {
 
 	public void ApplyGraphAdjustment()
 	{
-		vector23 = GenerateVector(rawPt2, rawPt3);
-		float stepSize = (vector12.magnitude * vector23.magnitude * vector13.magnitude) / (2 * Vector3.Cross(vector12, vector23).magnitude);
-		float pt1Coef = (Mathf.Pow(vector23.magnitude,2) * Vector3.Dot(vector12, vector13)) / (2 * Vector3.Cross(vector12, vector23).sqrMagnitude);
-		float pt2Coef = (Mathf.Pow(vector13.magnitude,2) * Vector3.Dot((-1) * vector12, vector23)) / (2 * Vector3.Cross(vector12, vector23).sqrMagnitude);
-		float pt3Coef = (Mathf.Pow(vector12.magnitude,2) * Vector3.Dot((-1) * vector13, (-1) * vector23)) / (2 * Vector3.Cross(vector12, vector23).sqrMagnitude);
-		float centerX = pt1Coef * rawPt1.X.Value + pt2Coef * rawPt2.X.Value + pt3Coef * rawPt3.X.Value;
-		float centerY = pt1Coef * rawPt1.Y.Value + pt2Coef * rawPt2.Y.Value + pt3Coef * rawPt3.Y.Value;
-		float centerZ = pt1Coef * rawPt1.Z.Value + pt2Coef * rawPt2.Z.Value + pt3Coef * rawPt3.Z.Value;
-		center = new Vector3(centerX, centerY, centerZ);
-		PtCoord centerPt = new PtCoord(new AxisCoord(centerX), new AxisCoord(centerY), new AxisCoord(centerZ));
-		//Get the range of the box
-		xLabelManager.Min = centerPt.X.Value - stepSize * steps;
-		yLabelManager.Min = centerPt.Y.Value - stepSize * steps;
-		zLabelManager.Min = centerPt.Z.Value - stepSize * steps;
-		xLabelManager.Max = centerPt.X.Value + stepSize * steps;
-		yLabelManager.Max = centerPt.Y.Value + stepSize * steps;
-		zLabelManager.Max = centerPt.Z.Value + stepSize * steps;
-		//Get the interaction points between the box edges and the plane
-		expr = solver.SymbolicateExpression(rawEquation);
-		getVerticesList();
+		if (PlaneValid()) {
+			print("lfhzlskjdhflkjs");
+			vector23 = GenerateVector(rawPt2, rawPt3);
+			float stepSize = (vector12.magnitude * vector23.magnitude * vector13.magnitude) / (2 * Vector3.Cross(vector12, vector23).magnitude);
+			float pt1Coef = (Mathf.Pow(vector23.magnitude,2) * Vector3.Dot(vector12, vector13)) / (2 * Vector3.Cross(vector12, vector23).sqrMagnitude);
+			float pt2Coef = (Mathf.Pow(vector13.magnitude,2) * Vector3.Dot((-1) * vector12, vector23)) / (2 * Vector3.Cross(vector12, vector23).sqrMagnitude);
+			float pt3Coef = (Mathf.Pow(vector12.magnitude,2) * Vector3.Dot((-1) * vector13, (-1) * vector23)) / (2 * Vector3.Cross(vector12, vector23).sqrMagnitude);
+			float centerX = pt1Coef * rawPt1.X.Value + pt2Coef * rawPt2.X.Value + pt3Coef * rawPt3.X.Value;
+			float centerY = pt1Coef * rawPt1.Y.Value + pt2Coef * rawPt2.Y.Value + pt3Coef * rawPt3.Y.Value;
+			float centerZ = pt1Coef * rawPt1.Z.Value + pt2Coef * rawPt2.Z.Value + pt3Coef * rawPt3.Z.Value;
+			center = new Vector3(centerX, centerY, centerZ);
+			//PtCoord centerPt = new PtCoord(new AxisCoord(centerX), new AxisCoord(centerY), new AxisCoord(centerZ));
+			//Get the range of the box
+			xLabelManager.Min = centerX - stepSize * steps;
+			yLabelManager.Min = centerY - stepSize * steps;
+			zLabelManager.Min = centerZ - stepSize * steps;
+			xLabelManager.Max = centerX + stepSize * steps;
+			yLabelManager.Max = centerY + stepSize * steps;
+			zLabelManager.Max = centerZ + stepSize * steps;
+			//Get the interaction points between the box edges and the plane
+			expr = solver.SymbolicateExpression(rawEquation);
+		}
 	}
 
 	public Vector3 GenerateVector(PtCoord pt1, PtCoord pt2)
@@ -137,65 +139,43 @@ public class PresentPlane : MonoBehaviour {
 			rawEquation = normalVector.x + "x+" + normalVector.y + "y+" + normalVector.z + "z=" + d;
 		}
 
+		point1.localPosition = ScaledPoint(PtCoordToVector(rawPt1));
+		print("PT1 location: " + point1.localPosition);
 
 		return rawEquation;
 	}
 
-	public bool PlaneValid() {
-		bool result = true;
-		
-		if (vector12.Equals(Vector3.zero) || vector13.Equals(Vector3.zero)) {
-			result = false;
+	public bool PlaneValid() {		
+		if (PtCoordToVector(rawPt1) == PtCoordToVector(rawPt2) || PtCoordToVector(rawPt1) == PtCoordToVector(rawPt3) || PtCoordToVector(rawPt2) == PtCoordToVector(rawPt3)) {
+			return false; 
 		}
 		
-		Vector3 temp = vector13 * (vector12.x / vector13.x);
+		float scale;
+		if (vector13.x != 0) {
+			scale = vector12.x / vector13.x;
+		} else if (vector13.y != 0) {
+			scale = vector12.y / vector13.y;
+		} else {
+			scale = vector12.z / vector13.z;
+		}
+		Vector3 temp = vector13 * scale;
 		if (vector12.Equals(temp)) {
-			result = false;
+			return false;
 		}
 
+		return true;
+	}
+
+	public Vector3 PtCoordToVector(PtCoord pt) {
+		return (new Vector3(pt.X.Value, pt.Y.Value, pt.Z.Value));
+	}
+
+	public Vector3 ScaledPoint(Vector3 pt) {
+		Vector3 result = Vector3.zero;
+		print("raw pt1 position: " + pt);
+		result.z = (pt.x - xLabelManager.Min) / (xLabelManager.Max - xLabelManager.Min) * 20 - 10;
+		result.x = (pt.y - yLabelManager.Min) / (yLabelManager.Max - yLabelManager.Min) * 20 - 10;
+		result.y = (pt.z - zLabelManager.Min) / (zLabelManager.Max - zLabelManager.Min) * 20 - 10;
 		return result;
-	}
-
-	public Vector3 CalculateX(float y, float z)
-	{
-		return new Vector3((- normalVector.y * y - normalVector.z * z + d) / normalVector.x, y, z);
-	}
-
-	public Vector3 CalculateY(float x, float z)
-	{
-		return new Vector3(x, (- normalVector.x * x - normalVector.z * z + d) / normalVector.y, z);
-	}
-
-	public Vector3 CalculateZ(float x, float y)
-	{
-		return new Vector3(x, y, (- normalVector.y * y - normalVector.x * x + d) / normalVector.z);
-	}
-
-	public bool PointWithinBox(float val, float min, float max)
-	{
-		return (val >= min) && (val <= max);
-	}
-
-	public void getVerticesList() 
-	{
-		List<Vector3> xVertices = new List<Vector3>();
-		xVertices.Add(CalculateX(yLabelManager.Min, zLabelManager.Min));
-		xVertices.Add(CalculateX(yLabelManager.Min, zLabelManager.Max));
-		xVertices.Add(CalculateX(yLabelManager.Max, zLabelManager.Max));
-		xVertices.Add(CalculateX(yLabelManager.Max, zLabelManager.Min));
-		xVertices.RemoveAll(point => !PointWithinBox(point.x, xLabelManager.Min, xLabelManager.Max));
-		List<Vector3> yVertices = new List<Vector3>();
-		yVertices.Add(CalculateY(xLabelManager.Min, zLabelManager.Min));
-		yVertices.Add(CalculateY(xLabelManager.Min, zLabelManager.Max));
-		yVertices.Add(CalculateY(xLabelManager.Max, zLabelManager.Max));
-		yVertices.Add(CalculateY(xLabelManager.Max, zLabelManager.Min));
-		yVertices.RemoveAll(point => !PointWithinBox(point.y, yLabelManager.Min, yLabelManager.Max));
-		List<Vector3> zVertices = new List<Vector3>();
-		zVertices.Add(CalculateZ(xLabelManager.Min, yLabelManager.Min));
-		zVertices.Add(CalculateZ(xLabelManager.Min, yLabelManager.Max));
-		zVertices.Add(CalculateZ(xLabelManager.Max, yLabelManager.Max));
-		zVertices.Add(CalculateZ(xLabelManager.Max, yLabelManager.Min));
-		zVertices.RemoveAll(point => !PointWithinBox(point.z, zLabelManager.Min, zLabelManager.Max));
-		
 	}
 }
