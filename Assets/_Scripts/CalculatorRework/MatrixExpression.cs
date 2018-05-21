204d9a721901867ce574821fa05e907e6393b821
mukmai <deaconkum@ymail.com>
2018-05-21 11:12:16 -0700

    Before Guanxin messing around with Deacon's code (actually she started messing already)

---
+++ Assets/_Scripts/CalculatorRework/MatrixExpression.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixExpression : MonoBehaviour 
+{
+	Scroll Scroll;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/_Scripts/GraphScripts/MatrixSet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MatrixSet 
+{
+	public Dictionary<string, bool> expValidity = new Dictionary<string, bool>();
+    public AK.ExpressionSolver solver = new AK.ExpressionSolver();
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
