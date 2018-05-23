5df96452fc1b0805141e743257eb06a282fe16bc
Ethan <edvander@ucsd.edu>
2018-05-23 09:59:08 -0700

    Found the bug with object deserialization. Objects are considered to be the same object if they are clones of eachother/same prefab.
    Going to attempt reversing the dictionary so that id maps to object instead.

--- Assets/_Scripts/GraphScripts/ExpressionSet.cs
+++ Assets/_Scripts/GraphScripts/ExpressionSet.cs
@@ -275,8 +275,6 @@ public abstract class CalcOutput //: ManualSerialize
         return true;
     }
 
-
-
     #region helper Functions
     /// <summary>
     /// checks if string represents a variable.
