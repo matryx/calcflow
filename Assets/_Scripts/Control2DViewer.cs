49cfbb4f496a3e3c9319fb3cf39b3503f68d1283
Ethan <edvander@ucsd.edu>
2018-07-19 16:07:23 -0700

    Added robot.

--- Assets/_Scripts/Control2DViewer.cs
+++ Assets/_Scripts/Control2DViewer.cs
@@ -34,7 +34,7 @@ public class Control2DViewer : MonoBehaviour
 
         Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - lastMouse);
         float speed = shiftMode ? slowDragSpeed : dragSpeed;
-        Vector3 move = new Vector3(-pos.y * speed, pos.x * speed);
+        Vector3 move = new Vector3(0,pos.x * speed, -pos.y * speed);
         //transform.Translate(move, Space.World);
         transform.eulerAngles += move;
     }
@@ -75,19 +75,19 @@ public class Control2DViewer : MonoBehaviour
         Vector3 p_Velocity = new Vector3();
         if (Input.GetKey(KeyCode.W))
         {
-            p_Velocity += new Vector3(0, 0, 1);
+            p_Velocity += new Vector3(-1, 0, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            p_Velocity += new Vector3(0, 0, -1);
+            p_Velocity += new Vector3(1, 0, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            p_Velocity += new Vector3(-1, 0, 0);
+            p_Velocity += new Vector3(0, 0, -1);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            p_Velocity += new Vector3(1, 0, 0);
+            p_Velocity += new Vector3(0, 0, 1);
         }
         if (Input.GetKey(KeyCode.Space))
         {
