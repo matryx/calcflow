51fd7d5fe26f9552582a3a8046d254746a703143
Ethan Brand <ethandbrand@gmail.com>
2018-08-20 14:12:44 -0700

    Add script to control expanding and contracting game object

--- Assets/_Scripts/MenuRework/ToggleSecondaryMenu.cs
+++ Assets/_Scripts/MenuRework/ToggleSecondaryMenu.cs
@@ -6,13 +6,10 @@ public class ToggleSecondaryMenu : QuickButton
 {
     Transform secondaryMenu;
     FuseButton fuseButton;
-    Vector3 menuScale, menuLoc;
-    Quaternion menuRot;
 
     protected override void Start()
     {
         base.Start();
-        menuScale = new Vector3(1f, 1f, 1f);
         SecondaryMenu secondary = SecondaryMenu.GetInstance();
         if (secondary != null)
         {
@@ -26,25 +23,16 @@ public class ToggleSecondaryMenu : QuickButton
         }
     }
 
-    void Update()
-    {
-        menuScale = secondaryMenu.localScale;
-        menuLoc = secondaryMenu.localPosition;
-        menuRot = secondaryMenu.localRotation;
-    }
 
     protected override void ButtonEnterBehavior(GameObject other)
     {
         if (secondaryMenu.gameObject.activeSelf)
         {
-            secondaryMenu.gameObject.SetActive(false);
+            StartCoroutine(secondaryMenu.GetComponent<ExpandContract>().Contract());
         }
         else
         {
-            secondaryMenu.gameObject.SetActive(true);
-            secondaryMenu.localPosition = menuLoc;
-            secondaryMenu.localRotation = menuRot;
-            StartCoroutine(ScaleTo(secondaryMenu, secondaryMenu.localScale, menuScale, .3f));
+            StartCoroutine(secondaryMenu.GetComponent<ExpandContract>().Expand());
         }
     }
 
@@ -53,20 +41,4 @@ public class ToggleSecondaryMenu : QuickButton
         //fuseButton.ForceCold();
     }
 
-    IEnumerator ScaleTo(Transform obj, Vector3 start, Vector3 end, float overTime)
-    {
-        float startTime = Time.time;
-
-        while (Time.time < startTime + overTime)
-        {
-            if (obj == null)
-            {
-                yield break;
-            }
-            obj.localScale = Vector3.Lerp(start, end, (Time.time - startTime) / overTime);
-            yield return null;
-        }
-
-        obj.localScale = end;
-    }
 }
