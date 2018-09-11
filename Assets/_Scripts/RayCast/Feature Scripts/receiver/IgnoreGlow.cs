508fc337363e971f494a1220a8e7292e188b29dd
Ethan Brand <ethandbrand@gmail.com>
2018-09-11 12:06:00 -0700

    Add helper script to ignore recursive glow on raycast

---
+++ Assets/_Scripts/RayCast/Feature Scripts/receiver/IgnoreGlow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreGlow : MonoBehaviour {
+
+    int layerIndex;
+    // Use this for initialization
+    void Start()
+    {
+        layerIndex = gameObject.layer;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (gameObject.layer == LayerMask.NameToLayer("Glow"))
+        {
+            gameObject.layer = layerIndex;
+        }
+	}
+}
