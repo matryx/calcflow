11c7a9065d5824fdaddf86b9ff9db4c5fbac831b
lostjam <jperdue@ucsd.edu>
2019-04-18 15:46:05 -0700

    set up both vectors and calculator. trying to map inputs.

---
+++ Assets/_Scripts/Orthogonal/rezOrthVec.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class rezOrthVec : MonoBehaviour
+{
+    //vector 1
+    public ConstraintGrabbable constraintGrabbable;
+    //take in whichever axis to project to 
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //do orthog math and transform result vector 
+        //transform.localPosition = constraintGrabbable.lastLocalPos;
+        transform.localPosition = constraintGrabbable.lastLocalPos;
+    }
+}
+
