bb4e86bdaea8b751c1cebd124bdb1ea35ac5bcfa
Tianheng (Matthew) Ma <37155540+tianhengMa@users.noreply.github.com>
2019-07-16 16:10:44 -0700

    Create RotateVector2.cs

---
+++ Assets/RotateVector2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateVector2 : MonoBehaviour {
+
+	public GameObject Point2 = GameObject.FindGameObjectWithTag("Point2");
+	public GameObject CubeOrigin = GameObject.FindGameObjectWithTag("CubeOrigin");
+	// Use this for initialization
+	void Start () {
+		//Vector3 temp = new Vector3(0.01f,0,0);
+		//gameObject.transform.position += temp;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 temp = new Vector3(0.01f,0,0);
+		//gameObject.transform.position += temp;
+	}
+}
