4af967eab1cc45d9654b6ba389e3d5833cddb3a0
Tianheng (Matthew) Ma <37155540+tianhengMa@users.noreply.github.com>
2019-07-25 11:45:08 -0700

    Created vector with arrows for each point

---
+++ Assets/_Scripts/Determinants/MoveVector3.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Determinants
+{
+    public class MoveVector3 : MonoBehaviour {
+        public Transform origin; //center
+        public PresentPlane presentplane;
+
+        void Update()
+        {
+                var mesh = GetComponentInChildren<MeshRenderer>().enabled;
+                transform.localPosition = presentplane.point3.localPosition;
+
+                //rendering boundaries
+                Vector3 position = transform.position;
+                if (origin.position - position != Vector3.zero)
+                {
+                    transform.rotation = Quaternion.LookRotation(origin.position - position);
+                }
+                //connect line from index 1 at 0 to the transform arrow tip that has local pos pt1
+                LineRenderer line = GetComponent<LineRenderer>();
+                line.SetWidth(.05f, .05f);
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, origin.position);
+
+                var sharedMaterial = transform.GetComponentInChildren<MeshRenderer>().sharedMaterial;
+                sharedMaterial.SetInt("_planeClippingEnabled", 1);
+
+                for (int i = 0; i < 6; i++)
+                {
+                    sharedMaterial.SetVector("_planePos" + i, presentplane.walls[i].transform.position);
+                    sharedMaterial.SetVector("_planeNorm" + i, presentplane.walls[i].transform.rotation * Vector3.up);
+                }
+
+        }
+    }
+}
