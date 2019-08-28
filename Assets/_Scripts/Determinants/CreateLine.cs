28c51d9dc13715e71dfe63075b146d40df2b86f4
Tianheng (Matthew) Ma <37155540+tianhengMa@users.noreply.github.com>
2019-08-28 15:48:40 -0700

    Added Border to PlaneSolver3D

---
+++ Assets/_Scripts/Determinants/CreateLine.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Determinants
+{
+	public class CreateLine : MonoBehaviour {
+		public Transform Point1;
+		public Transform Point2;
+		public Transform Point3;
+		public Transform origin;
+		public GameObject Line1;
+		public GameObject Line2;
+		public GameObject Line3;
+		public GameObject Line4;
+		public GameObject Line5;
+		public GameObject Line6;
+		public GameObject Line7;
+		public GameObject Line8;
+		public GameObject Line9;
+		public GameObject Line10;
+		public GameObject Line11;
+		public GameObject Line12;
+
+		// Update is called once per frame
+		void Update () {
+			//Get the positions of all the points
+			var position0 = origin.localPosition;
+			var position1 = Point1.localPosition;
+			var position2 = Point2.localPosition;
+			var position3 = Point3.localPosition;
+			var position4 = position1+position2;
+			var position5 = position1+position3;
+			var position6 = position2+position3;
+			var position7 = position1+position2+position3;
+
+			// Create borders
+			drawLine(position0,position1,Line1);
+			drawLine(position0,position2,Line2);
+			drawLine(position0,position3,Line3);
+			drawLine(position1,position4,Line4);
+			drawLine(position1,position5,Line5);
+			drawLine(position2,position4,Line6);
+			drawLine(position2,position6,Line7);
+			drawLine(position3,position5,Line8);
+			drawLine(position3,position6,Line9);
+			drawLine(position4,position7,Line10);
+			drawLine(position5,position7,Line11);
+			drawLine(position6,position7,Line12);
+		}
+
+		void drawLine(Vector3 startV, Vector3 endV, GameObject cylinder){
+			// Position
+			cylinder.transform.localPosition = (endV + startV)/2.0F;
+
+			// Rotation
+			cylinder.transform.localRotation = Quaternion.FromToRotation(Vector3.up, endV-startV);
+
+			// Scale        
+			float dist = Vector3.Distance(endV, startV);
+
+			cylinder.transform.localScale = new Vector3(0.1F, dist/2, 0.1F);
+		}
+	}
+}
--- Assets/_Scripts/Determinants/CreatePlane.cs
+++ Assets/_Scripts/Determinants/CreatePlane.cs
@@ -2,56 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreatePlane : MonoBehaviour {
-	public Transform FirstPoint;
-    public Transform SecondPoint;
-    public Transform Origin;
-	
-	// Use this for initialization
-	void Update () {
-        var firstposition = FirstPoint.localPosition;
-        var secondposition = SecondPoint.localPosition;
-        var originposition = Origin.localPosition;
+namespace Determinants
+{
+    public class CreatePlane : MonoBehaviour {
+        public Transform FirstPoint;
+        public Transform SecondPoint;
+        public Transform Origin;
+        
+        // Use this for initialization
+        void Update () {
+            var firstposition = FirstPoint.localPosition;
+            var secondposition = SecondPoint.localPosition;
+            var originposition = Origin.localPosition;
 
-		var mesh = new Mesh();
-        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+            var mesh = new Mesh();
+            gameObject.GetComponent<MeshFilter>().mesh = mesh;
 
-		var vertices = new Vector3[4]
-        {
-            originposition,
-            firstposition,
-            secondposition,
-            firstposition+secondposition
-        };
-        mesh.vertices = vertices;
+            var vertices = new Vector3[4]
+            {
+                originposition,
+                firstposition,
+                secondposition,
+                firstposition+secondposition
+            };
+            mesh.vertices = vertices;
 
-        var tris = new int[6]
-        {
-            // lower left triangle
-            0, 1, 2,
-            // upper right triangle
-            1, 3, 2,
-        };
-        mesh.triangles = tris;
+            var tris = new int[6]
+            {
+                // lower left triangle
+                0, 1, 2,
+                // upper right triangle
+                1, 3, 2,
+            };
+            mesh.triangles = tris;
 
-        var normals = new Vector3[4]
-        {
-            Vector3.forward,
-            Vector3.forward,
-            Vector3.forward,
-            Vector3.forward
-        };
-        mesh.normals = normals;
+            var normals = new Vector3[4]
+            {
+                Vector3.forward,
+                Vector3.forward,
+                Vector3.forward,
+                Vector3.forward
+            };
+            mesh.normals = normals;
 
-        var uv = new Vector2[4]
-        {
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(0, 1),
-            new Vector2(1, 1)
-        };
-        mesh.uv = uv;
-	}
-	
+            var uv = new Vector2[4]
+            {
+                new Vector2(0, 0),
+                new Vector2(1, 0),
+                new Vector2(0, 1),
+                new Vector2(1, 1)
+            };
+            mesh.uv = uv;
+        }
 
+        void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+            {
+                GameObject myLine = new GameObject();
+                myLine.transform.position = start;
+                myLine.AddComponent<LineRenderer>();
+                LineRenderer lr = myLine.GetComponent<LineRenderer>();
+                lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+                lr.SetColors(color, color);
+                lr.SetWidth(0.1f, 0.1f);
+                lr.SetPosition(0, start);
+                lr.SetPosition(1, end);
+                GameObject.Destroy(myLine, duration);
+            }
+        
+
+    }
 }
